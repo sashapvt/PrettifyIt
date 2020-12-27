9003152ab11671aa7634dd4a0013c27b02960e81
sashapvt <sashapvt@gmail.com>
2020-12-27 12:53:44 +0200

    Add project files.

---
+++ Shared/TextInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrettifyIt.Shared
+{
+    public class TextInputModel
+    {
+        [Required]
+        [StringLength(1000000, ErrorMessage = "Text is too long.")]
+        public string Text { get; set; }
+    }
+}
