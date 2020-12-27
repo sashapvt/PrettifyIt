ace444de7300b2cac09e2948ee5933b5da0eeca9
sashapvt <sashapvt@gmail.com>
2020-12-27 16:29:57 +0200

    First working version of JSON/XML formater

--- Formaters.cs
+++ Formaters.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -8,17 +9,52 @@ namespace PrettifyIt
 {
     public static class Formaters
     {
+        public static string Format(string text)
+        {
+            if (String.IsNullOrWhiteSpace(text)) return text;
+            var tText = text.Trim();
+            if (tText.StartsWith("<"))
+            {
+                return FormatXml(tText);
+            }
+            else if (tText.StartsWith("{"))
+            {
+                return FormatJson(tText);
+            }
+            else
+            {
+                return text;
+            }
+        }
+
         public static string FormatXml(string xml)
         {
             try
             {
-                XDocument doc = XDocument.Parse(xml);
-                return doc.ToString();
+                var doc = XDocument.Parse(xml);
+                return doc.Declaration != null
+                    ? doc.Declaration.ToString() + "\r\n" + doc.ToString()
+                    : doc.ToString();
             }
             catch (Exception)
             {
-                // Handle and throw if fatal exception here; don't just ignore them
                 return xml;
+            }
+        }
+
+        public static string FormatJson(string json)
+        {
+            try
+            {
+                var doc = JsonSerializer.Deserialize<dynamic>(json);
+                return JsonSerializer.Serialize(doc, new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+            }
+            catch (Exception)
+            {
+                return json;
             }
         }
     }
