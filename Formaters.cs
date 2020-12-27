14fd9e9d4edba13f36fa48e9065e9738ab6f4dd2
sashapvt <sashapvt@gmail.com>
2020-12-27 15:34:31 +0200

    XML formating

---
+++ Formaters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PrettifyIt
+{
+    public static class Formaters
+    {
+        public static string FormatXml(string xml)
+        {
+            try
+            {
+                XDocument doc = XDocument.Parse(xml);
+                return doc.ToString();
+            }
+            catch (Exception)
+            {
+                // Handle and throw if fatal exception here; don't just ignore them
+                return xml;
+            }
+        }
+    }
+}
